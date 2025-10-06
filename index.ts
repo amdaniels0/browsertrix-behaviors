fca8ed1402216ed060e19b6bccdd18585f452ed9
Alexander Daniels <alexdani@andrew.cmu.edu>
2025-10-06 11:14:07 -0400

    Add debug log to verify behaviors.js is being loaded

--- index.ts
+++ index.ts
@@ -1,2 +1,7 @@
 import { BehaviorManager } from "./src";
 export { BehaviorManager };
+
+// Debug log to confirm behaviors.js is loaded
+if (typeof console !== 'undefined' && console.log) {
+  console.log('[BROWSERTRIX-BEHAVIORS] behaviors.js loaded at', new Date().toISOString());
+}
